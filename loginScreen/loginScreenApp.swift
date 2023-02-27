58c68fc541df1b1db90f265caee1ce35277ad829
digiDreams <digital2dreamer@gmail.com>
2023-02-27 10:10:45 +0100

    Initial Commit

---
+++ loginScreen/loginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginScreenApp.swift
+//  loginScreen
+//
+//  Created by Apprenant 16 on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct loginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
